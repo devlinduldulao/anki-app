294b8de942a68d20235b3b5b5c4279333e98ebea
Devlin Duldulao <webmasterdevlin@gmail.com>
2026-01-29 14:42:30 +0100

    updated

---
+++ words7.ts
@@ -0,0 +1,38 @@
+type Question = {
+  norwegian: string;
+  english: string;
+  spanish: string;
+};
+
+export const words: Question[] = [
+  {
+    norwegian:'en forhåpentligvis',
+    english:'a hopefully',
+    spanish:'una esperanzadamente',
+  },
+  {
+    norwegian: 'ensomt',
+    english: 'lonely',
+    spanish: 'solitario',
+  },
+  {
+    norwegian: 'slippe å si',
+    english: 'avoid saying',
+    spanish: 'evitar decir',
+  },
+  {
+    norwegian: 'og opptatt',
+    english: 'and busy',
+    spanish: 'y ocupado',
+  },
+  {
+    norwegian: 'å trøste',
+    english: 'to comfort',
+    spanish: 'consolar',
+  },
+  {
+    norwegian: 'nemlig flest',
+    english: 'namely most',
+    spanish: 'es decir la mayoría',
+  }
+];
---
+++ words8.ts
@@ -0,0 +1,138 @@
+type Question = {
+  norwegian: string;
+  english: string;
+  spanish: string;
+};
+
+export const words: Question[] = [
+  {
+    norwegian: 'de omtalte',
+    english: 'the aforementioned',
+    spanish: 'los mencionados',
+  },
+  {
+    norwegian: 'rettferdihet',
+    english: 'justice',
+    spanish: 'justicia',
+  },
+  {
+    norwegian: 'en friere',
+    english: 'a suitor',
+    spanish: 'un pretendiente',
+  },
+  {
+    norwegian: 'ren utnyttelse',
+    english: 'pure exploitation',
+    spanish: 'pura explotación',
+  },
+  {
+    norwegian: 'er pålagt',
+    english: 'is imposed',
+    spanish: 'se impone',
+  },
+  {
+    norwegian: 'en kynisme',
+    english: 'a cynicism',
+    spanish: 'un cinismo',
+  },
+  {
+    norwegian: 'en egenrådighet',
+    english: 'a willfulness',
+    spanish: 'una voluntariedad',
+  },
+  {
+    norwegian: 'er uten sidestykke',
+    english: 'is unparalleled',
+    spanish: 'es inigualable',
+  },
+  {
+    norwegian: 'virksomhet',
+    english: 'activity',
+    spanish: 'actividad',
+  },
+  {
+    norwegian: 'norske myndigheter',
+    english: 'Norwegian authorities',
+    spanish: 'autoridades noruegas',
+  },
+  {
+    norwegian: 'jeg kjenner iallfall',
+    english: 'I know at least',
+    spanish: 'yo conozco al menos',
+  },
+  {
+    norwegian: 'bondefanget',
+    english: 'the dupe',
+    spanish: 'el engañado',
+  },
+  {
+    norwegian: 'å anse som en sekt',
+    english: 'to consider as a sect',
+    spanish: 'considerar como una secta',
+  },
+  {
+    norwegian: 'er faktum',
+    english: 'is a fact',
+    spanish: 'es un hecho',
+  },
+  {
+    norwegian: 'lotteritilsynet',
+    english: 'the lottery authority',
+    spanish: 'la autoridad de loterías',
+  },
+  {
+    norwegian: 'verve folk',
+    english: 'recruit people',
+    spanish: 'reclutar gente',
+  },
+  {
+    norwegian:'gevinstene',
+    english:'the winnings',
+    spanish:'las ganancias',
+  },
+  {
+    norwegian: 'i tvil om',
+    english: 'in doubt about',
+    spanish: 'en duda sobre',
+  },
+  {
+    norwegian: 'ble vervet gjennom',
+    english: 'was recruited through',
+    spanish: 'fue reclutado a través de',
+  },
+  {
+    norwegian: 'vilkårene',
+    english: 'the terms',
+    spanish: 'los términos',
+  },
+  {
+    norwegian: 'omtrent i',
+    english: 'approximately in',
+    spanish: 'aproximadamente en',
+  },
+  {
+    norwegian: 'å ta opp',
+    english: 'to record',
+    spanish: 'grabar',
+  },
+  {
+    norwegian: 'i skjul',
+    english: 'in hiding',
+    spanish: 'en escondite',
+  },
+  {
+    norwegian: 'å føre tilsyn med',
+    english: 'to supervise',
+    spanish: 'supervisar',
+  },
+  {
+    norwegian: 'skadevirkningene av',
+    english: 'the harmful effects of',
+    spanish: 'los efectos nocivos de',
+  },
+  {
+    norwegian: 'på å pålegge',
+    english: 'to impose on',
+    spanish: 'imponer a',
+  },
+];
