6a89bf45dc30c31c286d967592159e8d6658fafe
Devlin Duldulao <webmasterdevlin@gmail.com>
2026-02-01 10:13:22 +0100

    updated

--- words7.ts
+++ words7.ts
@@ -59,5 +59,150 @@ export const words: Question[] = [
     norwegian: 'bryr seg katta',
     english: "doesn't care a bit",
     spanish: 'no le importa en absoluto',
+  },
+  {
+    norwegian:'er artige',
+    english:'are funny',
+    spanish:'son divertidos',
+  },
+  {
+    norwegian: 'digitalt suverene',
+    english: 'digitally savvy',
+    spanish: 'digitalmente expertos',
+  },
+  {
+    norwegian: 'statsråden',
+    english: 'the minister',
+    spanish: 'el ministro',
+  },
+  {
+    norwegian: 'utopisk',
+    english: 'utopian',
+    spanish: 'utópico',
+  },
+  {
+    norwegian:'opplyser',
+    english:'informs',
+    spanish:'informa',
+  },
+  {
+    norwegian: 'det er bannlyst',
+    english: 'it is banned',
+    spanish: 'está prohibido',
+  },
+  {
+    norwegian: 'i overkant av',
+    english: 'in excess of',
+    spanish: 'en exceso de',
+  },
+  {
+    norwegian: 'kritisk truede',
+    english: 'critically endangered',
+    spanish: 'críticamente en peligro',
+  },
+  {
+    norwegian: 'er litt drøy',
+    english: 'is a bit much',
+    spanish: 'es un poco demasiado',
+  },
+  {
+    norwegian: 'på desken',
+    english: 'at the desk',
+    spanish: 'en el escritorio',
+  },
+  {
+    norwegian: 'er så lykkelig',
+    english: 'is so happy',
+    spanish: 'está tan feliz',
+  },
+  {
+   norwegian: 'det var jug',
+    english: 'it was a lie',
+    spanish: 'fue una mentira', 
+  },
+  {
+    norwegian: 'gjennomføre tiltak',
+    english: 'implement measures',
+    spanish: 'implementar medidas',
+  },
+  {
+    norwegian: 'skal slippe',
+    english: 'will avoid',
+    spanish: 'evitará',
+  },
+  {
+    norwegian: 'han truer',
+    english: 'he threatens',
+    spanish: 'él amenaza',
+  },
+  {
+    norwegian: 'beboerne der',
+    english: 'the residents there',
+    spanish: 'los residentes allí',
+  },
+  {
+    norwegian: 'på vegne av',
+    english: 'on behalf of',
+    spanish: 'en nombre de',
+  },
+  {
+    norwegian: 'et selvstyrt land',
+    english: 'a self-governing country',
+    spanish: 'un país autónomo',
+  },
+  {
+    norwegian: 'er på lag',
+    english: 'is on the team',
+    spanish: 'está en el equipo',
+  },
+  {
+    norwegian:'en hjerneforsker',
+    english:'a brain researcher',
+    spanish:'un investigador cerebral',
+  },
+  {
+    norwegian: 'i den anledning',
+    english: 'on that occasion',
+    spanish: 'en esa ocasión',
+  },
+  {
+    norwegian: 'på gradestokken',
+    english: 'on the thermometer',
+    spanish: 'en el termómetro',
+  },
+  {
+    norwegian: 'selgere bodene sine',
+    english: 'sell their stalls',
+    spanish: 'venden sus puestos',
+  },
+  {
+    norwegian: 'at myndehetene',
+    english: 'that the authorities',
+    spanish: 'que las autoridades',
+  },
+  {
+    norwegian: 'bekyttes',
+    english: 'are protected',
+    spanish: 'están protegidos',
+  },
+  {
+    norwegian: 'er utrydningstruet',
+    english: 'is endangered',
+    spanish: 'está en peligro de extinción',
+  },
+  {
+    norwegian: 'å øke bestanden',
+    english: 'to increase the population',
+    spanish: 'aumentar la población',
+  },
+  {
+    norwegian: 'av tap av leveområder',
+    english: 'due to loss of habitats',
+    spanish: 'debido a la pérdida de hábitats',
+  },
+  {
+    norwegian: 'i flosshatter til mannfolka',
+    english: 'in top hats for the men',
+    spanish: 'con sombreros de copa para los hombres',
   }
 ];
