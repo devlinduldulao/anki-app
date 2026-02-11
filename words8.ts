4c05bdc5a834c3adf027116b4b355807340ce4ae
Devlin Duldulao <webmasterdevlin@gmail.com>
2026-02-11 22:40:46 +0100

    updated

--- words8.ts
+++ words8.ts
@@ -135,4 +135,269 @@ export const words: Question[] = [
     english: 'to impose on',
     spanish: 'imponer a',
   },
+  {
+    norwegian: 'i fotsporene til',
+    english: 'in the footsteps of',
+    spanish: 'en los pasos de'
+  },
+  {
+    norwegian: 'til å stille',
+    english: 'to ask',
+    spanish: 'preguntar'
+  },
+  {
+    norwegian: 'meningsløse klesregler',
+    english: 'meaningless dress codes',
+    spanish: 'códigos de vestimenta sin sentido'
+  },
+  {
+    norwegian: 'ved store anledninger',
+    english: 'on big occasions',
+    spanish: 'en grandes ocasiones'
+  },
+  {
+    norwegian: 'er omvendt',
+    english: 'is reversed',
+    spanish: 'está invertido'
+  },
+  {
+    norwegian: 'uforskammet',
+    english: 'rude',
+    spanish: 'grosero'
+  },
+  {
+    norwegian: 'en diktaturregel',
+    english: 'a dictatorship rule',
+    spanish: 'una regla de dictadura'
+  },
+  {
+    norwegian: 'mye skryt',
+    english: 'a lot of praise',
+    spanish: 'muchos elogios'
+  },
+  {
+    norwegian: 'er berre',
+    english: 'is just',
+    spanish: 'es solo'
+  },
+  {
+    norwegian: 'med hemmet bevegelse',
+    english: 'with restricted movement',
+    spanish: 'con movimiento restringido'
+  },
+  {
+    norwegian: 'skulle ikke slutta',
+    english: 'should not have stopped',
+    spanish: 'no debería haber dejado de'
+  },
+  {
+    norwegian: 'forhandlingene om',
+    english: 'the negotiations about',
+    spanish: 'las negociaciones sobre'
+  },
+  {
+    norwegian: 'omfatter landets',
+    english: 'includes the country\'s',
+    spanish: 'incluye el país\'s'
+  },
+  {
+    norwegian: 'en vits på',
+    english: 'a joke about',
+    spanish: 'una broma sobre'
+  },
+  {
+    norwegian: 'det var revyer på',
+    english: 'there were revues on',
+    spanish: 'había revistas sobre'
+  },
+  {
+    norwegian: 'sensurerte',
+    english: 'censored',
+    spanish: 'censurado'
+  },
+  {
+    norwegian: 'et fristed',
+    english: 'a sanctuary',
+    spanish: 'un santuario'
+  },
+  {
+    norwegian: 'hele salen krymper seg',
+    english: 'the whole audience cringes',
+    spanish: 'todo el público se estremece'
+  },
+  {
+    norwegian: 'bikkja mi i går',
+    english: 'my dog yesterday',
+    spanish: 'mi perro ayer'
+  },
+  {
+    norwegian: 'en drabantby',
+    english: 'a suburb',
+    spanish: 'un suburbio'
+  },
+  {
+    norwegian: 'bare pælma',
+    english: 'just threw',
+    spanish: 'solo lanzó'
+  },
+  {
+    norwegian: 'provosert',
+    english: 'provoked',
+    spanish: 'provocado'
+  },
+  {
+    norwegian: 'blinklys',
+    english: 'turn signal',
+    spanish: 'señal de giro'
+  },
+  {
+    norwegian: 'leppestift',
+    english: 'lipstick',
+    spanish: 'lápiz labial'
+  },
+  {
+    norwegian: 'på trikken',
+    english: 'on the tram',
+    spanish: 'en el tranvía'
+  },
+  {
+    norwegian: 'et fugleskremsel',
+    english: 'a scarecrow',
+    spanish: 'un espantapájaros'
+  },
+  {
+    norwegian: 'omdefinere',
+    english: 'redefine',
+    spanish: 'redefinir'
+  },
+  {
+    norwegian: 'å trøste',
+    english: 'to comfort',
+    spanish: 'consolar'
+  },
+  {
+    norwegian: 'en bløtkake',
+    english: 'a cream cake',
+    spanish: 'un pastel de crema'
+  },
+  {
+    norwegian: 'av nettopp det ønsket',
+    english: 'of precisely that desire',
+    spanish: 'de precisamente ese deseo'
+  },
+  {
+    norwegian: 'litt bortskjemt',
+    english: 'a little spoiled',
+    spanish: 'un poco mimado'
+  },
+  {
+    norwegian: 'sluker det',
+    english: 'swallows it',
+    spanish: 'lo traga'
+  },
+  {
+    norwegian: 'sprudlende',
+    english: 'sparkling',
+    spanish: 'espumoso'
+  },
+  {
+    norwegian: 'samlei',
+    english: 'collected',
+    spanish: 'recogido'
+  },
+  {
+    norwegian: 'samvittighet',
+    english: 'conscience',
+    spanish: 'conciencia'
+  },
+  {
+    norwegian: 'innimellom',
+    english: 'in between',
+    spanish: 'entre'
+  },
+  {
+    norwegian: 'en dytt',
+    english: 'a push',
+    spanish: 'un empujón'
+  },
+  {
+    norwegian: 'løfter deg opp',
+    english: 'lifts you up',
+    spanish: 'te levanta'
+  },
+  {
+    norwegian: 'en jeg var sammen med',
+    english: 'a person I was with',
+    spanish: 'una persona con la que estuve'
+  },
+  {
+    norwegian: 'vippe meg opp på siden',
+    english: 'tip me up on the side',
+    spanish: 'inclinarme hacia un lado'
+  },
+  {
+    norwegian: 'å jodle',
+    english: 'to yodel',
+    spanish: 'yodelar'
+  },
+  {
+    norwegian: 'hakket',
+    english: 'a notch',
+    spanish: 'un nivel'
+  },
+  {
+    norwegian: 'hadde gruet seg',
+    english: 'had dreaded',
+    spanish: 'había temido'
+  },
+  {
+    norwegian: 'at du egentlig takler andre',
+    english: 'that you actually handle others',
+    spanish: 'que realmente manejas a los demás'
+  },
+  {
+    norwegian: 'å tørre',
+    english: 'to dare',
+    spanish: 'atreverse'
+  },
+  {
+    norwegian: 'et dikt',
+    english: 'a poem',
+    spanish: 'un poema'
+  },
+  {
+    norwegian: 'fjellfolk',
+    english: 'mountain people',
+    spanish: 'gente de montaña'
+  },
+  {
+    norwegian: 'ikke tviler på',
+    english: 'does not doubt',
+    spanish: 'no duda'
+  },
+  {
+    norwegian: 'en fryd',
+    english: 'a delight',
+    spanish: 'un deleite'
+  },
+  {
+    norwegian: 'som fyker',
+    english: 'that flies',
+    spanish: 'que vuela'
+  },
+  {
+    norwegian: 'idet',
+    english: 'as',
+    spanish: 'mientras'
+  },
+  {
+    norwegian: 'en lykkeligere',
+    english: 'a happier',
+    spanish: 'un más feliz'
+  },
+  {
+    norwegian: 'at vi turte',
+    english: 'that we dared',
+    spanish: 'que nos atrevimos'
+  }
 ];
