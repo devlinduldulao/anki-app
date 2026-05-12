5fb56b23c35719e2eb8f98bcfeca2e3e190ea566
Devlin Duldulao <webmasterdevlin@gmail.com>
2026-05-13 01:04:19 +0200

    updated

--- word11.ts
+++ word11.ts
@@ -4,4 +4,165 @@ type Question = {
   spanish: string;
 };
 
-export const words: Question[] = []
+export const words: Question[] = [
+  {
+    norwegian: 'svever',
+    english: 'floats',
+    spanish: 'flota'
+  },
+  {
+    norwegian: 'dust',
+    english: 'dust',
+    spanish: 'polvo'
+  },
+  {
+    norwegian: 'gjennomførte',
+    english: 'carried out',
+    spanish: 'llevado a cabo'
+  },
+  {
+    norwegian: 'titt',
+    english: 'peek',
+    spanish: 'mirar'
+  },
+  {
+    norwegian: 'Nålen',
+    english: 'The Needle',
+    spanish: 'La Aguja'
+  },
+  {
+    norwegian: 'en tåke',
+    english: 'a fog',
+    spanish: 'una niebla'
+  },
+  {
+    norwegian: 'Så pinlig',
+    english: 'So embarrassing',
+    spanish: 'Tan vergonzoso'
+  },
+  {
+    norwegian: 'Oppdiktet',
+    english: 'Fictional',
+    spanish: 'Ficticio'
+  },
+  {
+    norwegian: 'Å stanse',
+    english: 'To stop',
+    spanish: 'Detener'
+  },
+  {
+    norwegian: 'Har klikket',
+    english: 'Has clicked',
+    spanish: 'Ha hecho clic'
+  },
+  {
+    norwegian: 'Er knapp',
+    english: 'Is button',
+    spanish: 'Es botón'
+  },
+  {
+    norwegian: 'Gullsmed',
+    english: 'Goldsmith',
+    spanish: 'Orfebre'
+  },
+  {
+    norwegian: 'Utnytt',
+    english: 'Exploit',
+    spanish: 'Explotar'
+  },
+  {
+    norwegian: 'Begikk',
+    english: 'Committed',
+    spanish: 'Cometió'
+  },
+  {
+    norwegian: 'Dørslag',
+    english: 'Colander',
+    spanish: 'Colador'
+  },
+  {
+    norwegian: 'Snodig',
+    english: 'Weird',
+    spanish: 'Raro'
+  },
+  {
+    norwegian: 'Lystig',
+    english: 'Merry',
+    spanish: 'Alegre'
+  },
+  {
+    norwegian: 'Pinlig',
+    english: 'Embarrassing',
+    spanish: 'Vergonzoso'
+  },
+  {
+    norwegian: 'Forbrytelser',
+    english: 'Crimes',
+    spanish: 'Crímenes'
+  },
+  {
+    norwegian: 'Han varte lenger',
+    english: 'He lasted longer',
+    spanish: 'Él duró más'
+  },
+  {
+    norwegian: 'Ustoppelig',
+    english: 'Unstoppable',
+    spanish: 'Imparable'
+  },
+  {
+    norwegian: 'Avledning',
+    english: 'Diversion',
+    spanish: 'Desvío'
+  },
+  {
+    norwegian: 'Skyggen',
+    english: 'The Shadow',
+    spanish: 'La Sombra'
+  },
+  {
+    norwegian: 'Rampestreker',
+    english: 'Pranks',
+    spanish: 'Travesuras'
+  },
+  {
+    norwegian: 'Å begå',
+    english: 'To commit',
+    spanish: 'Cometer'
+  },
+  {
+    norwegian: 'Aksentee',
+    english: 'Accent',
+    spanish: 'Acento'
+  },
+  {
+    norwegian: 'Utslitt',
+    english: 'Exhausted',
+    spanish: 'Agotado'
+  },
+  {
+    norwegian: 'Dere lot meg',
+    english: 'You let me',
+    spanish: 'Me dejaron'
+  },
+  {
+    norwegian: 'i benk',
+    english: 'in the bench',
+    spanish: 'en el banco'
+  },
+  {
+    norwegian: 'Så sint',
+    english: 'So angry',
+    spanish: 'Tan enojado'
+  },
+  {
+    norwegian: 'Omringet',
+    english: 'Surrounded',
+    spanish: 'Rodeado'
+  },
+  {
+    norwegian: 'Overmanne deg',
+    english: 'Overcome you',
+    spanish: 'Superarte'
+  }
+]
