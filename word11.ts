27800a16e52696eba1668991bb799574045d0558
Devlin Duldulao <devlinduldulao@gmail.com>
2026-06-07 15:30:40 +0200

    updated

--- word11.ts
+++ word11.ts
@@ -164,5 +164,205 @@ export const words: Question[] = [
     norwegian: 'Overmanne deg',
     english: 'Overcome you',
     spanish: 'Superarte'
+  },
+  {
+    norwegian: 'samvittighet',
+    english: 'conscience',
+    spanish: 'conciencia'
+  },
+  {
+    norwegian: 'et forbilde',
+    english: 'a role model',
+    spanish: 'un modelo a seguir'
+  },
+  {
+    norwegian: 'inkasso',
+    english: 'debt collection',
+    spanish: 'cobranza'
+  },
+  {
+    norwegian:'bøller',
+    english: 'bullies',
+    spanish: 'matones'
+  },
+  {
+    norwegian: 'innbinding',
+    english: 'binding',
+    spanish: 'encuadernación'
+  },
+  {
+    norwegian: 'å skryte',
+    english: 'to brag',
+    spanish: 'presumir'
+  },
+  {
+    norwegian: 'rusten',
+    english: 'rusty',
+    spanish: 'oxidado'
+  },
+  {
+    norwegian: 'å steke meg',
+    english: 'to fry myself',
+    spanish: 'freírme'
+  },
+  {
+    norwegian: 'støtt',
+    english: 'support',
+    spanish: 'apoyo'
+  },
+  {
+    norwegian: 'er forgjengeren',
+    english: 'is the predecessor',
+    spanish: 'es el predecesor'
+  },
+  {
+    norwegian: 'avgjørende',
+    english: 'decisive',
+    spanish: 'decisivo'
+  },
+  {
+    norwegian: 'en slank',
+    english: 'a slender',
+    spanish: 'un delgado'
+  },
+  {
+    norwegian: 'utgift',
+    english: 'expense',
+    spanish: 'gasto'
+  },
+  {
+    norwegian: 'tross',
+    english: 'despite',
+    spanish: 'a pesar de'
+  },
+  {
+    norwegian: 'fordømt',
+    english: 'damned',
+    spanish: 'maldito'
+  },
+  {
+    norwegian: 'er ivrig',
+    english: 'is eager',
+    spanish: 'está ansioso'
+  },
+  {
+    norwegian: 'myslykket',
+    english: 'unsuccessful',
+    spanish: 'sin éxito'
+  },
+  {
+    norwegian: 'besverg',
+    english: 'swear',
+    spanish: 'jurar'
+  },
+  {
+    norwegian: 'en byrde',
+    english: 'a burden',
+    spanish: 'una carga'
+  },
+  {
+    norwegian: 'snåle',
+    english: 'weird',
+    spanish: 'raro'
+  },
+  {
+    norwegian: 'å tørne',
+    english: 'to crash',
+    spanish: 'chocar'
+  },
+  {
+    norwegian: 'å beskytte deg',
+    english: 'to protect yourself',
+    spanish: 'protegerte'
+  },
+  {
+    norwegian: 'en vidstrakt',
+    english: 'a vast',
+    spanish: 'un vasto'
+  },
+  {
+    norwegian: 'narrestrek',
+    english: 'prank',
+    spanish: 'broma'
+  },
+  {
+    norwegian: 'er pysa',
+    english: 'is a coward',
+    spanish: 'es un cobarde'
+  },
+  {
+    norwegian: 'overnaturlig',
+    english: 'supernatural',
+    spanish: 'sobrenatural'
+  },
+  {
+    norwegian: 'harefot',
+    english: 'harefoot',
+    spanish: 'pata de liebre'
+  },
+  {
+    norwegian: 'jeg adverte deg',
+    english: 'I warn you',
+    spanish: 'te advierto'
+  },
+  {
+    norwegian: 'forferdelig',
+    english: 'terrible',
+    spanish: 'terrible'
+  },
+  {
+    norwegian: 'pyser',
+    english: 'cowards',
+    spanish: 'cobardes'
+  },
+  {
+    norwegian: 'i tåken',
+    english: 'in the fog',
+    spanish: 'en la niebla'
+  },
+  {
+    norwegian: 'hva pokker',
+    english: 'what the heck',
+    spanish: 'qué demonios'
+  },
+  {
+    norwegian: 'like skjult',
+    english: 'just hidden',
+    spanish: 'simplemente oculto'
+  },
+  {
+    norwegian: 'hjemsøkt',
+    english: 'haunted',
+    spanish: 'embrujado'
+  },
+  {
+    norwegian: 'erter det nifne',
+    english: 'tease the spooky',
+    spanish: 'burlarse de lo espeluznante'
+  },
+  {
+    norwegian: 'stifter',
+    english: 'founder',
+    spanish: 'fundador'
+  },
+  {
+    norwegian: 'utkledd',
+    english: 'dressed up',
+    spanish: 'disfrazado'
+  },
+  {
+    norwegian: 'hun klamrer seg',
+    english: 'she clings',
+    spanish: 'ella se aferra'
+  },
+  {
+    norwegian: 'vi terningen sammen',
+    english: 'we roll the dice together',
+    spanish: 'lanzamos los dados juntos'
+  },
+  {
+    norwegian: 'nykommeren',
+    english: 'the newcomer',
+    spanish: 'el recién llegado'
   }
 ]
