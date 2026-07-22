35d6afb127d200bc9b288d78f946d2cf0c2c2aab
Devlin Duldulao <devlinduldulao@gmail.com>
2026-07-23 00:27:08 +0200

    updated

--- word12.ts
+++ word12.ts
@@ -4,4 +4,40 @@ type Question = {
   spanish: string;
 };
 
-export const words: Question[] = [];
+export const words: Question[] = [
+  {
+    norwegian: 'en svært dyktig snekker',
+    english: 'a very skilled carpenter',
+    spanish: 'un carpintero muy hábil'
+  },
+  {
+    norwegian: 'snekker',
+    english: 'carpenter',
+    spanish: 'carpintero'
+  },
+  {
+    norwegian: 'misnøye',
+    english: 'dissatisfaction',
+    spanish: 'descontento'
+  },
+  {
+    norwegian: 'etter båt-hendelse',
+    english: 'after boat incident',
+    spanish: 'después del incidente del barco'
+  },
+  {
+    norwegian: 'oppfylt',
+    english: 'fulfilled',
+    spanish: 'cumplido'
+  },
+  {
+    norwegian: 'et svært alvorlig funn',
+    english: 'a very serious finding',
+    spanish: 'un hallazgo muy grave'
+  },
+  {
+    norwegian: 'tålmodigheten',
+    english: 'the patience',
+    spanish: 'la paciencia'
+  }
+];
