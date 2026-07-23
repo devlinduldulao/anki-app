08c84d1aca11eae2743f630cc4ed827f6fede08c
Devlin Duldulao <devlinduldulao@gmail.com>
2026-07-23 17:19:57 +0200

    updated

--- word12.ts
+++ word12.ts
@@ -39,5 +39,60 @@ export const words: Question[] = [
     norwegian: 'tålmodigheten',
     english: 'the patience',
     spanish: 'la paciencia'
+  },
+  {
+    norwegian: 'usynlig i Norge',
+    english: 'invisible in Norway',
+    spanish: 'invisible en Noruega'
+  },
+  {
+    norwegian: 'utfordre bistandslauget',
+    english: 'challenge the aid guild',
+    spanish: 'desafiar al gremio de ayuda'
+  },
+  {
+    norwegian: 'kan ødelegge hørselen',
+    english: 'can damage hearing',
+    spanish: 'puede dañar la audición'
+  },
+  {
+    norwegian: 'oppskrift på saftig',
+    english: 'recipe for juicy',
+    spanish: 'receta para jugoso'
+  },
+  {
+    norwegian: 'du bo helt utenom det vanlige',
+    english: 'you live completely out of the ordinary',
+    spanish: 'vives completamente fuera de lo común'
+  },
+  {
+    norwegian: 'flåtten sprer seg',
+    english: 'the tick spreads',
+    spanish: 'la garrapata se propaga'
+  },
+  {
+    norwegian: 'dette må du passe på',
+    english: 'you have to watch out for this',
+    spanish: 'tienes que tener cuidado con esto'
+  },
+  {
+    norwegian: 'sammenlign forsikringer i 2026',
+    english: 'compare insurance in 2026',
+    spanish: 'compara seguros en 2026'
+  },
+  {
+    norwegian: 'bomkjøp',
+    english: 'bad purchase',
+    spanish: 'mala compra'
+  },
+  {
+    norwegian: 'nisse på lasset',
+    english: 'elf on the load',
+    spanish: 'elfo en la carga'
+  },
+  {
+    norwegian: 'behandles som forræderi',
+    english: 'treated as treason',
+    spanish: 'tratado como traición'
   }
 ];
